7463f6c96433d0cde10caaba7b42b8963968bb61
Sushanta Gupta <sushanta.gupta007@gmail.com>
2021-12-27 09:43:05 -0800

    footer component complete

--- my-app/src/Component/Footer/Footer.js
+++ my-app/src/Component/Footer/Footer.js
@@ -1,8 +1,11 @@
-import { Container, Grid, Typography} from '@mui/material';
+import { Avatar, Container, Grid, Typography} from '@mui/material';
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faFacebookF, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const useStyles = makeStyles({
     customLink: {
@@ -13,10 +16,24 @@ const useStyles = makeStyles({
         textDecoration:"underline",
       }
     },
-  });
+    
+})
+
+const customStyle = makeStyles({
+    customAvatar:{
+        backgroundColor:"white",
+        color:"#868383",
+        '&:hover':{
+            color:"#FFA902",
+        }
+    }
+})
+    
+ 
 
 const Footer = () => {
     const classes= useStyles()
+    const avatarClass = customStyle(); 
     return (
     <Container  sx={{ bgcolor: '#FFA902',color:"white",padding:"" }}>
         <Grid container spacing={2} sx={{padding:"10px"}}>
@@ -26,21 +43,43 @@ const Footer = () => {
                 </Typography>
             </Grid>
             <Grid item xs={4}>
-                <Link className={classes.customLink} to="https://www.facebook.com/">
+                <a className={classes.customLink}  href="https://web.facebook.com/">
                  Contact
-                </Link>
-                <Link to="/privacy" underline="hover">
-                    Privacy
-                </Link>
-                <Link to="/terms" underline="hover">
-                    Terms
-                </Link>
-                <Link to="/about" underline="hover">
-                    About
-                </Link>
+                </a>
+                <a className={classes.customLink}  href="https://web.facebook.com/">
+                 Privacy
+                </a>
+                <a className={classes.customLink}  href="https://web.facebook.com/">
+                 Terms 
+                </a>
+                <a className={classes.customLink}  href="https://web.facebook.com/">
+                 About
+                </a>
             </Grid>
             <Grid item xs={4}>
-            &copy; 2020 Travel Friend All Rights Reserved 
+                <Stack direction="row" spacing={2}>
+                    <a  href="https://web.facebook.com/"> 
+                        <Avatar   sx={{ width: 30, height: 30 }}  className={avatarClass.customAvatar} >
+                            <FontAwesomeIcon style={{width:"15px",height:"15px",color:"#FFA902"}}  icon={faFacebookF}></FontAwesomeIcon>
+                        </Avatar>
+                    </a>
+                    <a  href="https://web.facebook.com/"> 
+                        <Avatar   sx={{ width: 30, height: 30 }}  className={avatarClass.customAvatar} >
+                            <FontAwesomeIcon style={{width:"15px",height:"15px"}}  icon={faTwitter}></FontAwesomeIcon>
+                        </Avatar>
+                    </a>
+                    <a  href="https://web.facebook.com/"> 
+                        <Avatar   sx={{ width: 30, height: 30 }}  className={avatarClass.customAvatar} >
+                            <FontAwesomeIcon style={{width:"15px",height:"15px"}}  icon={faInstagram}></FontAwesomeIcon>
+                        </Avatar>
+                    </a>
+                    <a  href="https://web.facebook.com/"> 
+                        <Avatar   sx={{ width: 30, height: 30 }}  className={avatarClass.customAvatar} >
+                            <FontAwesomeIcon style={{width:"15px",height:"15px"}}  icon={faYoutube}></FontAwesomeIcon>
+                        </Avatar>
+                    </a>
+                    
+                </Stack>
             </Grid>
         </Grid>
     
